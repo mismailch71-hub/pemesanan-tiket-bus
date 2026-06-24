e192b186adb2c7623c1d890576e005fdf01f7287
m. ismail cahyadi <m.ismail.ch71@gmail.com>
2026-06-24 13:31:03 +0800

    push

--- app/Controllers/Penumpang.php
+++ app/Controllers/Penumpang.php
@@ -130,7 +130,7 @@ class Penumpang extends BaseController
         $transaksiModel = new \App\Models\TransaksiModel();
 
         $jadwal = $jadwalModel->select('jadwal.*, bus.kapasitas')
-                              ->join('bus', 'bus.id = jadwal.id', 'left')
+                              ->join('bus', 'bus.id = jadwal.id_bus', 'left')
                               ->find((int)$id_jadwal);
 
         if(!$jadwal) {
