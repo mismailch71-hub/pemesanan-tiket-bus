18cee3a560f7b745a1ef3a09fcd92298df644b58
m. ismail cahyadi <m.ismail.ch71@gmail.com>
2026-06-26 18:38:01 +0800

    fix allowedFields id_bus di JadwalModel

--- app/Models/JadwalModel.php
+++ app/Models/JadwalModel.php
@@ -9,6 +9,6 @@ class JadwalModel extends Model
 {
     protected $table = 'jadwal';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['nama_bus','asal','tujuan','jam_keberangkatan','tanggal_keberangkatan','harga','total_kursi'];
+    protected $allowedFields = ['nama_bus','asal','tujuan','jam_keberangkatan','tanggal_keberangkatan','harga','total_kursi', 'id_bus'];
 
 }
